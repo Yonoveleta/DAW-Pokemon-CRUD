e24f006db2d1cbbb89a97b9157f4a087ff458b64
Yonoveleta <thetruevayne@gmail.com>
2024-10-20 21:12:43 +0200

    Initial commit

---
+++ script/pokemon.js
@@ -0,0 +1,14 @@
+pokemons = [
+    {
+        name: "Pikachu",
+        types: ["Electric"]
+    },
+    {
+        name: "Garchomp",
+        types: ["Dragon", "Ground"]
+    }
+]
+
+function getPokemons() {
+    return pokemons;
+}
